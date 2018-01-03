0fe9f86a553239b29334161f6bb8a7b5876eb983
Disha <dishamendiratta@gmail.com>
2018-01-03 15:27:25 +0530

    deleted directory

--- restaurants/dish/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-import models
-
-# Register your models here.
-
-admin.site.register(models.Restaurant)
-admin.site.register(models.Dish)
-admin.site.register(models.RestaurantReview)
